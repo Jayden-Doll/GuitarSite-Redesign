5530e447cf19a17e1a258b8ed96b0ec1ebe809e9
Jayden Doll <jayjdoll002@gmail.com>
2022-01-24 17:29:41 -0800

    Rewrite and refactor guitar sound functionality

--- scripts/app.js
+++ scripts/app.js
@@ -52,29 +52,49 @@ sliderBackground.addEventListener("click", () => {
 
 */
 
-//Add sound file names to corresponding arrays
-const stringSounds = ["E", "A", "D", "G", "B", "e"];
-const stringSoundsC = ["MutedE", "C3", "E2", "G", "C1", "e"];
-const stringSoundsG = ["G3", "B2", "D", "G", "D3", "g3"];
-const stringSoundsD = ["E", "A", "D", "A2", "D3", "ff2"];
-const stringSoundsA = ["E", "A", "E2", "A2", "CC2", "e"];
-const stringSoundsE = ["E", "B2", "E2", "GG1", "B", "e"];
-const stringSoundsF = ["F1", "C3", "F3", "A2", "C1", "f1"];
+const menuItems = document.querySelectorAll(".menu-item");
+
+//Highlights a menu item when clicked on
+const selectItem = () => {
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      menuItems.forEach((item) => {
+        if (item.classList.contains("selected")) {
+          item.classList.remove("selected");
+        }
+      });
+      item.classList.add("selected");
+    });
+  });
+};
+
+selectItem();
 
 //Select buttons and container
 const container = document.querySelector(".guitar-strings");
-const btnDefault = document.querySelector(".btn-default");
-const btnC = document.querySelector(".btn-c");
-const btnG = document.querySelector(".btn-g");
-const btnD = document.querySelector(".btn-d");
-const btnA = document.querySelector(".btn-a");
-const btnE = document.querySelector(".btn-e");
-const btnF = document.querySelector(".btn-f");
+const btnDefault = document.querySelector("#default");
+const btnA = document.querySelector(".btn-A");
+const btnC = document.querySelector(".btn-C");
+const btnD = document.querySelector(".btn-D");
+const btnE = document.querySelector(".btn-E");
+const btnF = document.querySelector(".btn-F");
+const btnG = document.querySelector(".btn-G");
 
-//Default notes setup
-const defaultStrings = () => {
-  //Loop over each item in the string array, make each sound a div, and append it to the container
-  stringSounds.forEach((stringSound) => {
+const allSounds = {
+  default: ["E", "A", "D", "G", "B", "e"],
+  A: ["E", "A", "E2", "A2", "CC2", "e"],
+  C: ["MutedE", "C3", "E2", "G", "C1", "e"],
+  D: ["E", "A", "D", "A2", "D3", "ff2"],
+  E: ["E", "B2", "E2", "GG1", "B", "e"],
+  F: ["F1", "C3", "F3", "A2", "C1", "f1"],
+  G: ["G3", "B2", "D", "G", "D3", "g3"],
+};
+
+const generateStringSounds = (chord) => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  allSounds[chord].forEach((stringSound) => {
     const div = document.createElement("div");
     div.classList.add("string");
 
@@ -86,176 +106,32 @@ const defaultStrings = () => {
   });
 };
 
-defaultStrings();
+generateStringSounds("default");
 
-//When on another chord, press the default button to go back to default strings setup
-const ifDefaultButtonPressed = () => {
-  btnDefault.addEventListener("click", () => {
-    //Delete the first child of the container until there are no children
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
+btnDefault.addEventListener("click", () => {
+  generateStringSounds("default");
+});
 
-    //Switches to the default string setup
-    defaultStrings();
-  });
-};
+btnA.addEventListener("click", () => {
+  generateStringSounds("A");
+});
 
-//C Chord Function
-const playChordC = () => {
-  btnC.addEventListener("click", () => {
-    //On click, remove every child
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
+btnC.addEventListener("click", () => {
+  generateStringSounds("C");
+});
 
-    //Creates template for C chord note as similarly to the default strings function above
-    stringSoundsC.forEach((stringSoundC) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
+btnD.addEventListener("click", () => {
+  generateStringSounds("D");
+});
 
-      div.innerText = stringSoundC;
+btnE.addEventListener("click", () => {
+  generateStringSounds("E");
+});
 
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundC}`).play();
-      });
+btnF.addEventListener("click", () => {
+  generateStringSounds("F");
+});
 
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    //refer to the ifDefaultButtonPressed function
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordC();
-
-//G Chord Function
-const playChordG = () => {
-  btnG.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    stringSoundsG.forEach((stringSoundG) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
-
-      div.innerText = stringSoundG;
-
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundG}`).play();
-      });
-
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordG();
-
-//D Chord Function
-const playChordD = () => {
-  btnD.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    stringSoundsD.forEach((stringSoundD) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
-
-      div.innerText = stringSoundD;
-
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundD}`).play();
-      });
-
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordD();
-
-//A Chord Function
-const playChordA = () => {
-  btnA.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    stringSoundsA.forEach((stringSoundA) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
-
-      div.innerText = stringSoundA;
-
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundA}`).play();
-      });
-
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordA();
-
-//E Chord Function
-const playChordE = () => {
-  btnE.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    stringSoundsE.forEach((stringSoundE) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
-
-      div.innerText = stringSoundE;
-
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundE}`).play();
-      });
-
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordE();
-
-//F Chord Function
-const playChordF = () => {
-  btnF.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-
-    stringSoundsF.forEach((stringSoundF) => {
-      const div = document.createElement("div");
-      div.classList.add("container--sound-item");
-
-      div.innerText = stringSoundF;
-
-      div.addEventListener("mouseover", () => {
-        document.querySelector(`#${stringSoundF}`).play();
-      });
-
-      document.querySelector(".container--sound-list").appendChild(div);
-    });
-
-    ifDefaultButtonPressed();
-  });
-};
-
-playChordF();
+btnG.addEventListener("click", () => {
+  generateStringSounds("G");
+});
